7633821395b9b0d40d695d225ca15e44e33ffdd6
044ireri <jamlickmuriuki65@gmail.com>
2025-11-03 11:42:25 +0300

    Add files via upload

---
+++ program_to_prompt_the_user_their_age.cpp
@@ -0,0 +1,18 @@
+# include <stdio.h>
+//name: jamlick muriuki 
+//Reg no: 26542
+//age: 18
+int main () {
+int age;
+//inputs 
+printf("enter your age");
+scanf("%d",& age);
+return 0;
+}
+
+    
+    
+    
+    
+
+     
