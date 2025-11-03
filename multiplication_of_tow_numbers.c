3607391cc4b42b6a28e88e1e8b542f281d0ca3b0
044ireri <jamlickmuriuki65@gmail.com>
2025-11-03 11:40:51 +0300

    Add files via upload

---
+++ multiplication_of_tow_numbers.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+//name: jamlick muriuki 
+//reg no:26542
+//age:18 years 
+int main(){
+// multiplication of two numbers 
+int num1,num2, multiple;
+//ask the user to enter two numbers 
+printf("enter first number");
+scanf("%d", &num1);
+printf("enter the second number");
+scanf("%d", &num2);
+multiple=num1*num2;
+//display results 
+printf("multiple is %d \n", num1*num2, multiple);
+return 0;
+}
+
+    
