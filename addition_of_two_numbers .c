3bf09d0aeb5aaa6bf24f6963be5fac3f68230eea
044ireri <jamlickmuriuki65@gmail.com>
2025-11-03 11:40:02 +0300

    Add files via upload

---
+++ addition_of_two_numbers .c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+//name: jamlick muriuki 
+//reg no:26542
+//age:18 years 
+int main (){
+//addition of two numbers 
+int num1,num2,sum;
+//ask the user to enter two numbers 
+printf("enter the first number:");
+scanf("%d", &num1);
+printf("enter the second number:");
+scanf("%d", &num2);
+//calculate the sum
+sum=num1+num2;
+//display results 
+printf("sum is %d\n",num1+num2,sum);
+return 0;
+}
+
